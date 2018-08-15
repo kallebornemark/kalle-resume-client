938c002ba79eb74aceaf05eceaf420c96acd4fd5
Kalle Bornemark <kalle.bornemark@hiq.se>
2018-08-15 17:16:57 +0200

    Remove no-alert rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-alert': 0,
     'max-len': ["error", { "code": 135 }],
     'linebreak-style': 0
   },
