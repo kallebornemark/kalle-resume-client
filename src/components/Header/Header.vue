e0ea7f612c624d9c4b52fd2f0b2fe1a304adaed4
Kalle Bornemark <kalle.bornemark@gmail.com>
2018-11-10 11:36:53 +0100

    Create RolldownBlinds component

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -2,11 +2,7 @@
   <div class="header">
     <h1 class="name">Kalle Bornemark</h1>
 
-    <header-line />
-
-    <transition name="fade-bounce" appear>
-      <h4 class="subtitle">Web Developer</h4>
-    </transition>
+    <rolldown-blinds text="web developer"/>
 
     <login />
   </div>
@@ -15,61 +11,30 @@
 <script>
 import Login from '@/components/Login.vue';
 import HeaderLine from '@/components/Header/HeaderLine.vue';
+import RolldownBlinds from '@/components/RolldownBlinds.vue';
 
 export default {
   name: 'Header',
-  components: { Login, HeaderLine },
+  components: { Login, HeaderLine, RolldownBlinds },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 @import '~@/styles/vars.scss';
-@import '~@/styles/vars.scss';
 
 .header {
+  height: 12rem;
   display: flex;
-  /* justify-content: center; */
-  align-items: center;
   flex-direction: column;
-  height: 15rem;
   text-align: center;
-  /* position: relative; */
+  margin-bottom: 6rem;
 
   .name {
-    color: $color-primary;
-    font-family: $font-primary;
-    font-size: 3.3em;
-    text-align: center;
-    line-height: 4.7rem;
-    padding: 0 1rem;
-    margin: 3.5rem 0 0 0;
-  }
-
-  .subtitle {
-    position: absolute;
-    top: 9.5rem;
-    color: transparentize($color: $color-primary, $amount: .25);
-    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI";
-    letter-spacing: 4px;
-    text-transform: lowercase;
-    font-size: .95em;
-    font-weight: 400;
-    margin: 0;
+    margin-top: 5rem;
+    color: $color-athens-gray;
+    font-size: 2.2em;
   }
 }
 
-.fade-bounce-enter-active, {
-  transition: opacity .55s cubic-bezier(.87,-.41,.19,1.44), top .5s cubic-bezier(.87,-.41,.19,1.44);
-  /* transition: opacity .5s, top; */
-  transition-delay: .3s;
-}
-.fade-bounce-enter, {
-  opacity: 0;
-  top: 7rem !important;
-}
-.fade-bounce-enter-to {
-  opacity: 1;
-  top: 9.5rem;
-}
 </style>
---
+++ src/components/RolldownBlinds.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="rolldown-blinds">
+      <div class="line" />
+
+      <transition name="slide-down" appear>
+        <div class="shadow" />
+      </transition>
+
+      <transition name="fade-bounce" appear>
+        <h4 class="subtitle">{{text}}</h4>
+      </transition>
+    </div>
+</template>
+
+<script>
+export default {
+  props: ['text'],
+};
+</script>
+
+<style lang="scss">
+@import '~@/styles/vars.scss';
+
+.rolldown-blinds {
+  /* width: 12.92rem; */
+  width: 11.6rem;
+  margin: 0 auto;
+  max-width: 100%;
+  position: relative;
+
+  .line {
+    box-sizing: border-box;
+    background-color: $color-azure-radiance;
+    height: 2px;
+    width: 100%;
+    /* border-left: 2px solid $color-barley-white; */
+    /* border-right: 2px solid $color-barley-white; */
+    border-radius: 1px;
+  }
+
+  .shadow {
+    background-color: $color-ebony-clay-2;
+    opacity: .5;
+    height: 2.9rem;
+  }
+
+  .subtitle {
+    position: absolute;
+    left: 0; right: 0; top: .9rem;
+    color: $color-regent-st-blue;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI";
+    letter-spacing: 3px;
+    text-transform: lowercase;
+    font-size: .95em;
+    font-weight: 400;
+    margin: 0;
+  }
+}
+
+// Text
+.slide-down-enter-active {
+  transition: height 0.4s cubic-bezier(0.455, 0.505, 0, 1.22);
+  transition-delay: 0.65s;
+}
+.slide-down-enter {
+  height: 0 !important;
+}
+.slide-down-enter-to {
+  height: 2.3rem;
+}
+
+// Blinds
+.fade-bounce-enter-active {
+  transition: opacity 0.55s cubic-bezier(0.87, -0.41, 0.19, 1.44),
+    top .55s cubic-bezier(0.87, -0.41, 0.19, 1.44);
+  /* transition: opacity .5s, top; */
+  transition-delay: 0.5s;
+}
+.fade-bounce-enter {
+  opacity: 0;
+  top: .5rem !important;
+}
+.fade-bounce-enter-to {
+  opacity: 1;
+  top: .9rem;
+}
+</style>
